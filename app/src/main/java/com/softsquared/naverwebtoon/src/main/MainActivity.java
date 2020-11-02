4868310be6280a6247e787984e84102048856fbf
gyetol <econostein@naver.com>
2020-11-02 22:19:45 +0900

    메인액티비티 프래그먼트 틀 완료, sticky scrollview library 테스트중

--- app/src/main/java/com/softsquared/naverwebtoon/src/main/MainActivity.java
+++ app/src/main/java/com/softsquared/naverwebtoon/src/main/MainActivity.java
@@ -1,16 +1,61 @@
 package com.softsquared.naverwebtoon.src.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.softsquared.naverwebtoon.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentWebtoon fragmentWebtoon = new FragmentWebtoon();
+    private FragmentRecommendation fragmentRecommendation = new FragmentRecommendation();
+    private FragmentBestchallenge fragmentBestchallenge = new FragmentBestchallenge();
+    private FragmentMypage fragmentMypage = new FragmentMypage();
+    private FragmentMore fragmentMore = new FragmentMore();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentWebtoon).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction =fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId()){
+                case R.id.bottom_navigation_view_webtoon_1:
+                    transaction.replace(R.id.frameLayout,fragmentWebtoon).commitAllowingStateLoss();
+                    break;
+                case R.id.bottom_navigation_view_recommendation_2:
+                    transaction.replace(R.id.frameLayout,fragmentRecommendation).commitAllowingStateLoss();
+                    break;
+                case R.id.bottom_navigation_view_bestchallenge_3:
+                    transaction.replace(R.id.frameLayout,fragmentBestchallenge).commitAllowingStateLoss();
+                    break;
+                case R.id.bottom_navigation_view_my_4:
+                    transaction.replace(R.id.frameLayout,fragmentMypage).commitAllowingStateLoss();
+                    break;
+                case R.id.bottom_navigation_view_more_5:
+                    transaction.replace(R.id.frameLayout,fragmentMore).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 }
